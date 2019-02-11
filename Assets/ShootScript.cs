aaa7f6217f0e0cfe92c5562b3650cb066ced91e8
DavidR <david.rowsellwork@gmail.com>
2019-02-11 20:37:08 +1100

    Commit v1.5

--- Assets/ShootScript.cs
+++ Assets/ShootScript.cs
@@ -1,22 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class ShootScript : MonoBehaviour
+public class ShootScript : NetworkBehaviour
 {
     //fire rate of weapon
     public float fireRate = 1f;
     //range of weapon
     public float fireRange = 100f;
-
+    //LayerMask we wil be hitting
     public LayerMask mask;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    //timer for the fire rate
+    private float fireFactor = 0f;
+    //the main cam
+    private GameObject mainCam;
+    // Use this for initialization
+    void Start()
+    {
+        mainCam.GetComponentInChildren<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    [Command]
+    void Cmd_PlayerShot(string _id)
+    {
+        Debug.Log("Player" + _id + "has been shot!");
+    }
+    [Client]
+    void Shoot()
+    {
+        RaycastHit rayBullet;
+        Physics.Raycast(mainCam.transform.position, mainCam.transform.forward, out rayBullet, fireRange, mask);
+
+
+    }
 }
